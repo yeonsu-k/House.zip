506516197aad69bc41e64df7081fc79e61390f31
Kuuuna98 <yunaghgh@naver.com>
2022-05-21 21:18:08 +0900

    정렬

--- src/components/notice/NoticeList.vue
+++ src/components/notice/NoticeList.vue
@@ -4,7 +4,13 @@
     <b-button v-if="isManager" variant="outline-success" style="float: right; width: auto" to="/notice/regist">글쓰기</b-button>
     <hr />
     <div class="text-right">
-      <small class="mb-3 text-right">* 전체 공지사항 수 : {{ noticeCnt }} </small>
+      <b-row>
+        <b-col cols="8"></b-col>
+        <b-col cols="2"><b-form-select v-model="selected" :options="options" size="sm" class="mt-3" @change="sort"></b-form-select></b-col>
+        <b-col align-self="end" cols="2">
+          <small class="mb-3 text-right">* 전체 공지사항 수 : {{ noticeCnt }} </small>
+        </b-col>
+      </b-row>
     </div>
     <div v-if="notices.length">
       <b-table-simple hover striped responsive class="text-center">
@@ -43,19 +49,48 @@ export default {
   },
   data() {
     return {
+      selected: "최신순",
+      options: [
+        { value: "최신순", text: "최신순" },
+        { value: "오래된순", text: "오래된순" },
+        { value: "제목순", text: "제목순" },
+        { value: "조회수순", text: "조회수순" },
+      ],
       notices: {
         type: Array,
       },
     };
   },
-  methods: {},
+  methods: {
+    sort() {
+      let sortCal = "regtime";
+      let sortVal = "desc";
+
+      switch (this.selected) {
+        case "오래된순":
+          sortVal = "asc";
+          break;
+        case "제목순":
+          sortCal = "title";
+          sortVal = "asc";
+          break;
+        case "조회수순":
+          sortCal = "hit";
+          // sortVal = "desc";
+          break;
+      }
+      axios.get("http://localhost:8080/happyhouse/notice/", { params: { sortCal, sortVal } }).then(({ data }) => {
+        this.notices = data;
+      });
+    },
+  },
   computed: {
     noticeCnt() {
       return this.notices.length;
     },
   },
   created() {
-    axios.get("http://localhost:8080/happyhouse/notice/").then(({ data }) => {
+    axios.get("http://localhost:8080/happyhouse/notice/", { params: { sortCal: "regtime", sortVal: "desc" } }).then(({ data }) => {
       this.notices = data;
     });
   },
