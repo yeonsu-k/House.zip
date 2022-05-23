81a1f1ef81e7e828b07432e9286db6caa89129c0
dustnzlzl <dustn4325@naver.com>
2022-05-24 01:26:16 +0900

    intro 삭제

--- src/components/IntroView.vue
+++ src/components/IntroView.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="intro">
     <!-- Header-->
-    <b-jumbotron>
-      <div>
-        <h1 class="font-weight-bold display-4" style="text-shadow: 1px 1px 1px #999">Happy House</h1>
-        <hr />
-        <span style="font-size: 25px"><h4>introduce</h4></span>
-      </div>
-    </b-jumbotron>
+
     <!-- Main Content-->
     <div class="container mb-4">
       <div class="text-center">
@@ -27,28 +21,4 @@
 export default {};
 </script>
 
-<style scoped>
-.jumbotron {
-  height: 40vh;
-  text-align: center;
-  position: relative;
-  z-index: 1;
-}
-.jumbotron::after {
-  width: 100%;
-  height: 100%;
-  content: "";
-  /* background: url("@/assets/img/home-bg.jpg"); */
-  background-position: center;
-  background-size: fill;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  opacity: 0.65;
-}
-.jumbotron > div {
-  width: 70%;
-  display: inline-block;
-}
-</style>
+<style scoped></style>
