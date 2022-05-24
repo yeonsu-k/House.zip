67d87d7388458b5e9e35aafd0c015e222f7e52c6
dustnzlzl <dustn4325@naver.com>
2022-05-24 09:48:17 +0900

    footer배치 수정

--- src/components/common/FooterView.vue
+++ src/components/common/FooterView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <b-row align-h="center" class="gx-4 gx-lg-5" style="height: 10vh">
+    <b-row align-h="center" class="gx-4 gx-lg-5 pt-3" style="height: 35px">
       <b-col align-self="center" md="10" class="col-lg-8 col-xl-7">
         <div class="small text-center text-muted fst-italic mb-2">Happy House Project</div>
         <div class="small text-center text-muted fst-italic">&copy; SSAFY 7기 권유나,김연수</div>
