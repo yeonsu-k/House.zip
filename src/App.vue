d30802f75ee0552bebaca0d0051490caacb3d216
Kuuuna98 <yunaghgh@naver.com>
2022-05-26 20:48:52 +0900

    챗봇 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,22 @@
     <header-nav :loginId="loginId" :loginName="loginName" @logout="logout"></header-nav>
     <router-view :loginId="loginId" @logout="logout" @login="login"></router-view>
     <footer-view v-if="$route.name != 'house' && $route.name != 'HouseAll'"></footer-view>
+
+    <div
+      id="frogue-container"
+      class="position-right-bottom"
+      data-chatbot="49e527c3-689f-49f9-9a2e-ba06d6ee1b07"
+      data-user="사용자ID"
+      data-init-key="value"
+      data-close-use="Y"
+      data-close-bgcolor="#ffffff"
+      data-close="#000000"
+      data-move-use="Y"
+      data-move-bgcolor="#ffffff"
+      data-move="#000000"
+    ></div>
+    <!-- data-init-식별키=값 으로 셋팅하면 챗플로우에 파라미터와 연동가능. 식별키는 소문자만 가능 -->
+    <!-- data 속성 매뉴얼 https://doc.danbee.ai/channel_frogu.html -->
   </div>
 </template>
 
@@ -20,6 +36,17 @@ export default {
     if (this.loginId) {
       this.loginName = localStorage.getItem("loginName");
     }
+    ((d, s, id) => {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https:danbee.ai/js/plugins/frogue-embed/frogue-embed.min.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "frogue-embed");
   },
   data() {
     return {
